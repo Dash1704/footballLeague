9de1852b3ae0953301e829ca6db84e89956eb69c
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-05-16 11:34:28 +0100

    DB is now connected

--- server/app.js
+++ server/app.js
@@ -8,6 +8,12 @@ require('dotenv').config();
 const app = express();
 
 //db
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+})
+.then(() => console.log('DB CONNECTED'))
+.catch(err => console.log('DB CONNECTION ERROR', err));
 
 
 // middleware
