900bbd596b92799424c4ff4828d32b2cf2e80f81
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-05-24 15:32:32 +0100

    Refactored league controllers

--- server/controllers/leagues.js
+++ server/controllers/leagues.js
@@ -5,15 +5,19 @@ const getLeagues = asyncHandler(async (req, res) => {
 })
 
 const postLeagues = asyncHandler(async (req, res) => {
+  if(!req.body.text){
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
   res.status(200).json({ message: 'Post a new league'})
 })
 
 const updateLeagues = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Update a league'})
+  res.status(200).json({ message: `Update league ${req.params.id}`})
 })
 
 const deleteLeagues = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Delete a league'})
+  res.status(200).json({ message: `Delete league ${req.params.id}`})
 })
 
 module.exports = {
