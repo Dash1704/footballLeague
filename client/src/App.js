5316db9d75b22dbc7ca667d1c289748fd9d7f7dd
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-05-26 13:50:03 +0100

    Add leage button on the client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React, {Fragment} from "react";
 import './App.css';
 
+//components
+import InputLeague from "./components/inputLeague";
+
 function App() {
-  return <Fragment></Fragment>
-}
+  return (
+    <Fragment>
+      <div className="container">
+      <InputLeague/>
+      </div>
+    </Fragment>
+    )
+  }
 
 export default App;
